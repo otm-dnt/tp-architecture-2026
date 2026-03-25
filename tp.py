0b2b174baa0a98c97492f4fb6fc78eb8b55a04e9
Othmane Dounit <166843345+otm-dnt@users.noreply.github.com>
2026-03-25 13:24:08 +0100

    ajout de rank_matter_1 et affichage

--- tp.py
+++ tp.py
@@ -15,6 +15,11 @@ class SchoolClass:
 
     def add_student(self, student):
         self.students.append(student)
+    def rank_matter_1(self):
+        sorted_students = sorted(self.students, key=lambda s: s.m1, reverse=True)
+        print("\nClassement matière 1 :")
+        for student in sorted_students:
+            print(f"{student.name}: {student.m1}")
         
 if __name__ == "__main__":
     # Création d'une instance de la classe
@@ -22,8 +27,12 @@ if __name__ == "__main__":
     school_class.add_student(Student('J', 10, 12, 13))
     school_class.add_student(Student('A', 8, 2, 17))
     school_class.add_student(Student('V', 9, 14, 14))
-
     # Affichage des moyennes pour vérifier
     print("Moyennes des étudiants :")
     for student in school_class.students:
         print(f"{student.name}: moyenne = {student.average()}")
+
+    # Tri matière 1
+    school_class.rank_matter_1()
+
+  
